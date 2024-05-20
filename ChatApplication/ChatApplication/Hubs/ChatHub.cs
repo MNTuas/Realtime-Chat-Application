3fd4ff431e7f4c1ee8b596e4a0ee381d03bfe9ca
Minh Tuan <tuannmse161231@fpt.edu.vn>
2024-05-20 22:49:42 +0700

    code base

---
+++ ChatApplication/ChatApplication/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using ChatApplication.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApplication.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(UserConnection conn)
+        {
+            await Clients.All.SendAsync(method: "ReciveMessage", arg1: "admin", arg2:$"{conn.Username} has joined");
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection conn)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName: conn.ChatRoom);
+            await Clients.Groups(conn.ChatRoom)
+                .SendAsync(method: "ReciveMessage", arg1: "admin", arg2: $"{conn.Username} has joined {conn.ChatRoom}");
+        }
+    }
+}
