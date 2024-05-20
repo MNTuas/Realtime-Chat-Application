1fb87b8a698fd9672d6305a8292c7e478438331a
Minh Tuan <tuannmse161231@fpt.edu.vn>
2024-05-20 23:35:12 +0700

    add ui component

--- uichatapp/src/App.js
+++ uichatapp/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import {Col,Container,Row} from "react-bootstrap"
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import WaitingRoom from './components/waitingroom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <Container>
+          <Row class='px-5 my-5'>
+            <Col sm='12'>
+              <h1 classname='font-weight-light'>Welcome too the Chat App</h1>
+            </Col>
+          </Row>
+          <WaitingRoom></WaitingRoom>
+        </Container>
+      </main>
     </div>
   );
 }
